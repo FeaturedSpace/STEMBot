9fac2b1da37fe33db55c92a552bc0012e9c66753
Noah <noahcoder77@gmail.com>
2019-01-16 11:13:16 -0500

    Name Change

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -40,7 +40,7 @@ exports.config = {
 
 exports.help = {
   name: 'disconnect',
-  description: 'Disconnect Bastion from any voice connection in the Discord server.',
+  description: 'Disconnect STEMBot from any voice connection in the Discord server.',
   botPermission: '',
   userTextPermission: '',
   userVoicePermission: '',
